ae3c397bac058ae04944c97513a8612006cdc3f7
Trufanov Nikita <trufanovwork@gmail.com>
2022-04-18 21:20:34 +0300

    lala

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -2,7 +2,26 @@
 
 require_relative "ipgeobase/version"
 
+require 'happymapper'
+require 'addressable/template'
+require 'net/http'
+
 module Ipgeobase
-  class Error < StandardError; end
-  # Your code goes here...
+  class Post
+    include HappyMapper
+    tag 'query'
+    element :city, String, tag: 'city'
+    element :country, String, tag: 'country'
+    element :countryCode, String, tag: 'countryCode'
+    element :lat, String, tag: 'lat'
+    element :lon, String, tag: 'lon'
+  end
+  class Error < StandardError
+  end
+  def self.lookup(ip)
+    template = Addressable::Template.new("http://{host}{/type}/{ip}")
+    uri = template.expand({ 'host' => 'ip-api.com', 'type' => 'xml', 'ip' => ip })
+    get_data = Net::HTTP.get_response(uri)
+    posts = Post.parse(get_data.body)
+  end
 end
